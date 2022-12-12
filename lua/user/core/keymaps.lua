8f34c07c2d3c214871fd6cb31e0e16820fb61798
serii <seriiburduja@gmail.com>
2022-12-12 20:54:17 +0200

    explorer: toggle works

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -29,10 +29,11 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+keymap.set("n", "<C-h>", "<C-w>h") -- close current split window
+keymap.set("n", "<C-j>", "<C-w>j") -- close current split window
+keymap.set("n", "<C-k>", "<C-w>k") -- close current split window
+keymap.set("n", "<C-l>", "<C-w>l") -- close current split window
+
 
 -- " formatting in normal mode
 -- nmap <Leader>f <Plug>(prettier-format)
@@ -57,7 +58,8 @@ vim.g.multi_cursor_quit_key = '<Esc>'
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
 
 -- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>e", ":NvimTreeFindFile<CR>") -- toggle file explorer
+keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>") -- toggle file explorer
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
--- lua/user/setup-plugins.lua
+++ lua/user/setup-plugins.lua
@@ -46,7 +46,7 @@ return packer.startup(function(use)
   -- essential plugins
   use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
   -- use("vim-scripts/ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
-
+  -- use('neoclide/coc.nvim')
   -- commenting with gc
   use("numToStr/Comment.nvim")
 
