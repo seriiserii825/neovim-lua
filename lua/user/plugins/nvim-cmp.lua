0c922ba0cdaae4a8ffb8399bb2f8c458e93e6501
serii <seriiburduja@gmail.com>
2022-12-13 11:07:53 +0200

    cmp c-j

--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -25,7 +25,7 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C->"] = cmp.mapping.select_next_item(), -- next suggestion
+    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
     ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
