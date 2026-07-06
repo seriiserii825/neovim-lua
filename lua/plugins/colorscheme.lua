de16627ca8bdd52ed187a1717def20e357425fbf
serii burduja <seriiburduja@gmail.com>
2026-07-06 09:07:11 +0300

    feat: colorscheme bg

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,11 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require("nordic").setup({
+        on_highlight = function(highlights)
+          highlights.Visual = { bg = "#3d59a1" }
+        end,
+      })
       require("nordic").load()
     end,
   },
